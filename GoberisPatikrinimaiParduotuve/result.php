6239f5a07f39c8aeaf15bdc5df89e26bf005a686
xeffire <justinasgoberis@gmail.com>
2021-03-06 13:09:02 +0200

    fix redirect result to main after clicking view results

--- GoberisPatikrinimaiParduotuve/result.php
+++ GoberisPatikrinimaiParduotuve/result.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-if (!isset($_SESSION['avg'])) header('Location: main.php');
+if (!isset($_COOKIE['avg'])) header('Location: main.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
