2911ac6923c67d64c22812c795ff7481a2c2cbef
xeffire <justinasgoberis@gmail.com>
2021-03-11 21:19:26 +0200

    Premature upload for Ligthhouse testing #2

--- CCSGrids/js.js
+++ CCSGrids/js.js
@@ -1,99 +1,45 @@
 function Ops() {
   this.getNumberBtns = function () {
     return [
-      this.one,
-      this.two,
-      this.three,
-      this.four,
-      this.five,
-      this.six,
-      this.seven,
-      this.eight,
-      this.nine,
-      this.zero,
+      this["1"],
+      this["2"],
+      this["3"],
+      this["4"],
+      this["5"],
+      this["6"],
+      this["7"],
+      this["8"],
+      this["9"],
+      this["0"],
     ];
   };
   this.getOpBtns = function () {
-    return [this.add, this.sub, this.mul, this.div, this.point];
+    return [this["+"], this["-"], this["×"], this["÷"], this["."]];
   };
   this.getFuncBtns = function () {
     return [this.ac, this.del, this.equal];
   };
   for (let btn of document.getElementsByTagName("button")) {
-    switch (btn.innerText) {
-      case "AC":
-        this.ac = btn;
-        break;
-      case "DEL":
-        this.del = btn;
-        break;
-      case "÷":
-        this.div = btn;
-        break;
-      case "1":
-        this.one = btn;
-        break;
-      case "2":
-        this.two = btn;
-        break;
-      case "3":
-        this.three = btn;
-        break;
-      case "×":
-        this.mul = btn;
-        break;
-      case "4":
-        this.four = btn;
-        break;
-      case "5":
-        this.five = btn;
-        break;
-      case "6":
-        this.six = btn;
-        break;
-      case "+":
-        this.add = btn;
-        break;
-      case "7":
-        this.seven = btn;
-        break;
-      case "8":
-        this.eight = btn;
-        break;
-      case "9":
-        this.nine = btn;
-        break;
-      case "-":
-        this.sub = btn;
-        break;
-      case ".":
-        this.point = btn;
-        break;
-      case "0":
-        this.zero = btn;
-        break;
-      case "=":
-        this.equal = btn;
-        break;
-      default:
-        break;
-    }
+    this[btn.innerText] = btn;
   }
-
+  console.log(this);
 }
 
 let ops = new Ops();
 let lastSymbol = "0";
 let isLastNumFloat = false;
 let freshCalc = true;
-let last = document.getElementById('last');
+let last = document.getElementById("last");
 let curr = document.getElementById("curr");
 
 const assingFunctions = () => {
   for (let btn of [...ops.getNumberBtns(), ...ops.getOpBtns()]) {
     btn.addEventListener("click", appendSymbol);
   }
-  ops.equal.addEventListener("click", calc);
+  ops["="].addEventListener("click", calc);
+  ops.AC.addEventListener("click", clear);
+  ops.DEL.addEventListener("click", delet);
+  document.addEventListener("keydown", keyboardNav);
 };
 
 const appendSymbol = (event) => {
@@ -118,20 +64,21 @@ const appendSymbol = (event) => {
     return;
   }
 
-  //(if last symb is operator and new symb is also) or (last is 0 and new is 0) - remove last symb before adding new
-  if ((isOp && isLastOp) || (!isOp && curr.innerText === "0")) {
+  //if last symb is operator and new symb is also
+  //or last is 0 and new is 0 - remove last symb before adding new
+  if ((isOp && isLastOp) || (!isOp && lastSymbol === "0" && !isLastNumFloat)) {
     curr.innerHTML = curr.innerText.slice(0, -1);
   }
 
   //if btn is operator but not '.': isFloat = false;
-  if (isOp && btn.innerText != '.') {
-    isLastNumFloat = false
+  if (isOp && btn.innerText != ".") {
+    isLastNumFloat = false;
   }
 
   curr.innerText += btn.innerText;
 
   // lastSymbol = curr.innerText[curr.innerText.length-1] == /\d/?btn.innerText:lastSymbol;
-  lastSymbol = curr.innerText[curr.innerText.length-1];
+  lastSymbol = curr.innerText[curr.innerText.length - 1];
   freshCalc = false;
 };
 
@@ -143,12 +90,14 @@ const calc = () => {
     console.log(curr.innerText);
   }
 
-  last.innerText = curr.innerText + ' =';
+  last.innerText = curr.innerText + " =";
 
   // split numbers and operators to separate arrays.
-  let numArr = curr.innerText.split(/[+\-×÷]/g).map(i => parseFloat(i));
-  let opArr = curr.innerText.split(/[\d.]+/g).filter((el) => {return el != ''});
-  console.log(curr.innerText + "   " + numArr + '   ' + opArr);
+  let numArr = curr.innerText.split(/[+\-×÷]/g).map((i) => parseFloat(i));
+  let opArr = curr.innerText.split(/[\d.]+/g).filter((el) => {
+    return el != "";
+  });
+  console.log(curr.innerText + "   " + numArr + "   " + opArr);
 
   //if first number arrays starts with NaN (first symbol is -): turn first num negative, fix order of nums and ops.
   if (isNaN(numArr[0])) {
@@ -158,8 +107,7 @@ const calc = () => {
     opArr.splice(0, 1);
   }
 
-
-  //Do operations till one number left. 
+  //Do operations till one number left.
   while (numArr.length > 1) {
     let i = opArr.indexOf("×");
     if (i > -1) {
@@ -190,11 +138,46 @@ const calc = () => {
       continue;
     }
   }
-  curr.innerText = numArr[0].toFixed(6).replace(/\.?0*$/,'');;
-  last.innerText += numArr[0].toFixed(6).replace(/\.?0*$/,'');;
+  curr.innerText = numArr[0].toFixed(6).replace(/\.?0*$/, "");
+  last.innerText += numArr[0].toFixed(6).replace(/\.?0*$/, "");
   lastSymbol = curr.innerText;
-  isLastNumFloat = lastSymbol.search(/\./) > 0?true:false;
+  isLastNumFloat = lastSymbol.search(/\./) > 0 ? true : false;
   freshCalc = true;
+};
+
+const clear = () => {
+  curr.innerText = "0";
+  last.innerText = "0";
+  lastSymbol = "0";
+};
+
+const delet = () => {
+  curr.innerText = curr.innerText.slice(0, -1);
+  if (curr.innerText.length === 0) {
+    curr.innerText = "0";
+  }
+  lastSymbol = curr.innerText.substr(-1, 1);
+};
+
+const keyboardNav = (event) => {
+  let btn = ops[event.key];
+  switch (event.key) {
+    case "Enter":
+      btn = ops["="];
+      break;
+    case "Backspace":
+      btn = ops["DEL"];
+      break;
+      case "Delete": 
+      btn = ops['AC'];
+      break;
+    default:
+      break;
+  }
+  if (btn == null) {
+    return;
+  }
+  btn.click();
 };
 
 assingFunctions();
